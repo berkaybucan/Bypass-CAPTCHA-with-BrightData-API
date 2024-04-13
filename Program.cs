354bdb98189d9f5836eb788b962ce4bd4bd6f81d
Kerim Berkay Buçan <sp0wnberkay@gmail.com>
2024-04-14 01:57:41 +0300

    a

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bypassCaptchaBrightData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+            var client = new WebClient();
+            client.Proxy = new WebProxy("###");
+            client.Proxy.Credentials = new NetworkCredential("productId", "productPass");
+            HtmlWeb web = new HtmlWeb();
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(client.DownloadString("siteurl"));
+            if (htmlDocument != null)
+            {
+
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
